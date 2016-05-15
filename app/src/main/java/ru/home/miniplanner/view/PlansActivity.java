70366105dc5bf593a6330a0d8eb24b4f5bdba9e3
privod <nikolay_dvn@rambler.ru>
2016-05-15 12:02:16 +0700

    Начало реализации селектора

--- app/src/main/java/ru/home/miniplanner/view/PlansActivity.java
+++ app/src/main/java/ru/home/miniplanner/view/PlansActivity.java
@@ -1,6 +1,7 @@
 package ru.home.miniplanner.view;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -31,16 +34,27 @@ public class PlansActivity extends AppCompatActivity {
     public static final int REQUEST_PARTIES = 10;
     private static final int REQUEST_PLAN_EDIT = 20;
 
+    private static enum Mode {
+        NORMAL, EDIT, REMOVE
+    }
+
+    private static Mode mode;
+
     PlanDao planDao;
     PlanAdapter planAdapter;
     RecyclerView recyclerView;
+    Toolbar toolbar;
 //    List<Plan> planSelected;
+
+    MenuItem editMenuItem;
+    MenuItem removeMenuItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plans);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         HelperFactory.setHelper(this);
@@ -56,6 +70,8 @@ public class PlansActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.plans_appearance);
+        recyclerView.startAnimation(anim);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -64,9 +80,6 @@ public class PlansActivity extends AppCompatActivity {
                 openPlanEditActivity(0);
             }
         });
-
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.plans_appearance);
-        recyclerView.startAnimation(anim);
     }
 
     @Override
@@ -99,13 +112,97 @@ public class PlansActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_PLAN_EDIT);
     }
 
+    private int getSelectedPlanCount(List<Plan> plans) {
+        int result = 0;
+        for (Plan plan: plans) {
+            if (plan.isSelected()) { result += 1; }
+        }
+        return result;
+    }
+
+    private void startMode(Mode modeToStart) {
+        if (modeToStart == Mode.NORMAL) {
+            editMenuItem.setVisible(false);
+            removeMenuItem.setVisible(false);
+            toolbar.setLogo(null);
+//            toolbar.setTitle(getString(R.string.title_plans));
+            toolbar.setBackgroundResource(R.color.colorPrimary);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        } else if (modeToStart == Mode.EDIT) {
+            editMenuItem.setVisible(true);
+            removeMenuItem.setVisible(true);
+            toolbar.setLogo(R.drawable.ic_keyboard_backspace_white_24dp);
+//            toolbar.setTitle(getString(R.string.title_plans));
+            toolbar.setBackgroundResource(R.color.colorSelect);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.colorSelectDark));
+            }
+        } else if (modeToStart == Mode.REMOVE) {
+            editMenuItem.setVisible(false);
+            removeMenuItem.setVisible(true);
+            toolbar.setLogo(R.drawable.ic_keyboard_backspace_white_24dp);
+//            toolbar.setTitle(getString(R.string.title_plans));
+            toolbar.setBackgroundResource(R.color.colorSelect);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.colorSelectDark));
+            }
+        }
+    }
+
+    private Mode getModeBySelected(List<Plan> plans) {
+        if (getSelectedPlanCount(plans) == 0) {
+            startMode(Mode.NORMAL);
+            toolbar.setTitle(getString(R.string.title_plans));
+        } else {
+            if (getSelectedPlanCount(plans) == 1) {
+                startMode(Mode.EDIT);
+            } else {
+                startMode(Mode.REMOVE);
+                toolbar.setTitle(String.valueOf(getSelectedPlanCount(plans)));
+            }
+        }
+
+    }
+
     public void planSelect(View view, int position) {
 
-        Plan plan = planDao.getAll().get(position);
+        List<Plan> plans = planDao.getAll();
+        Plan plan = plans.get(position);
         plan.setSelected(!plan.isSelected());
         planDao.save(plan);
         AvatarLetterView avatarLetterView = (AvatarLetterView) view.findViewById(R.id.avatarLetter);
         avatarLetterView.AnimationSwitchSelectedState();
+
+
+//        toolbar.setLogo(R.drawable.ic_keyboard_backspace_white_24dp);
+//        toolbar.setBackgroundResource(R.color.colorSelect);
+
+//        final ImageView selectorImage  = (ImageView) view.findViewById(R.id.selectorImage);
+//        final Animation animToSide = AnimationUtils.loadAnimation(this, R.anim.to_side);
+//        final Animation animFromSide = AnimationUtils.loadAnimation(this, R.anim.from_side);
+//        animToSide.setAnimationListener( new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) { }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                avatarLetterView.setVisibility(View.INVISIBLE);
+//                selectorImage.setVisibility(View.VISIBLE);
+//                selectorImage.startAnimation(animFromSide);
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) { }
+//        });
+//        avatarLetterView.startAnimation(animToSide);
     }
 
     @Override
@@ -127,6 +224,11 @@ public class PlansActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        editMenuItem = menu.findItem(R.id.action_edit);
+        removeMenuItem = menu.findItem(R.id.action_remove);
+
+        startMode(Mode.NORMAL);
 
         return true;
     }
