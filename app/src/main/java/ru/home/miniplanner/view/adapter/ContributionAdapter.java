16522518599bd4c0c0e081b1ed515dffdb6f3753
privod <bespalov@krsk.ustech.ru>
2017-05-17 22:24:40 +0700

    Обновлен ContributionAdapter

--- app/src/main/java/ru/home/miniplanner/view/adapter/ContributionAdapter.java
+++ app/src/main/java/ru/home/miniplanner/view/adapter/ContributionAdapter.java
@@ -1,43 +1,60 @@
 package ru.home.miniplanner.view.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bignerdranch.android.multiselector.MultiSelector;
+
 import ru.home.miniplanner.R;
+import ru.home.miniplanner.Util;
 import ru.home.miniplanner.model.Contribution;
+import ru.home.miniplanner.model.Plan;
 
 /**
  * Created by privod on 19.10.2015.
  */
-public class ContributionAdapter extends OldBaseAdapter<Contribution> {
-    static final String LOG_TAG = ContributionAdapter.class.getSimpleName();
+public class ContributionAdapter extends BaseAdapter<ContributionAdapter.ContributionViewHolder, Contribution> {
 
-    public ContributionAdapter(Context context) {
-        super(context);
+    public ContributionAdapter(MultiSelector multiSelector) {
+        super(multiSelector/*, PlanViewHolder.class*/);
+    }
+
+    class ContributionViewHolder extends BaseAdapter.ViewHolder {
+        private TextView descriptionTextView;
+        private TextView dateRegTextView;
+        private TextView sumTextView;
+//        private AvatarViewSwitcher avatarViewSwitcher;
+        private ImageView avatarIcon;
+
+        ContributionViewHolder(View itemView) {
+            super(itemView);
+
+            descriptionTextView = (TextView) itemView.findViewById(R.id.text_view_description);
+            dateRegTextView = (TextView) itemView.findViewById(R.id.text_view_date_reg);
+            sumTextView = (TextView) itemView.findViewById(R.id.text_view_sum);
+            avatarIcon = (ImageView) itemView.findViewById(R.id.icon_avatar);
+        }
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
+    public ContributionAdapter.ContributionViewHolder onCreateViewHolder(ViewGroup parent, int viewType)  {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.plan_view, parent, false);
 
-        if (null == view) {
-            view = getLayout().inflate(R.layout.contribution_view, parent, false);
-        }
+        return new ContributionAdapter.ContributionViewHolder(view);
+    }
 
-        Contribution contribution = (Contribution) getItem(position);
+    @Override
+    public void onBindViewHolder(ContributionAdapter.ContributionViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
 
-//        TextView descriptionTextView = (TextView) view.findViewById(R.id.descriptionTextView);
-        TextView partyTextView = (TextView) view.findViewById(R.id.descriptionTextView);
-        TextView dateRegTextView = (TextView) view.findViewById(R.id.text_view_date_reg);
-        TextView sumTextView = (TextView) view.findViewById(R.id.sumTextView);
-
-//        getViewService().textViewSetText(descriptionTextView, contribution.getDescription());
-        getViewService().textViewSetText(partyTextView, contribution.getTo().getName());
-        getViewService().textViewSetText(dateRegTextView, contribution.getDateReg());
-        getViewService().textViewSetText(sumTextView, contribution.getSum());
-
-        return view;
+        Contribution contribution = getData().get(position);
+        holder.descriptionTextView.setText(contribution.getDescription());
+        holder.dateRegTextView.setText(Util.dateToString(contribution.getDateReg()));
+        holder.sumTextView.setText(contribution.getSum().toPlainString());
+        holder.avatarIcon.setImageDrawable(newAvatarDrawable(contribution.getDescription()));
     }
 }
